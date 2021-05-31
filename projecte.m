acb01555e50aa7b7150a95dbdc0753f83377e73a
ArnauDeu <82084067+ArnauDeu@users.noreply.github.com>
2021-06-01 01:43:07 +0200

    UPLOAD DATASET i CODI

--- projecte.m
+++ projecte.m
@@ -8,50 +8,133 @@ close all, clear all
 
 %% TASCA 1 - SELECCIÓ i IMPORTACIÓ DEL DATASET 
 % En aquesta part hem gestionat la carga de les dades
-% i també com emmagatzemarem les dades.
-%
+% i també com emmagatzemarem les dades. A més separem els frames 
+% inicials que s'utilitzaràn pel train
 
 cd C:\Users\ernu1\Desktop\Universitat\PSIV\PROJECTE
 
-v = VideoReader("video2.mp4");
+%v = VideoReader("video1-1.mp4");
+%v = VideoReader("video1-2.mp4");
+%v = VideoReader("video1-3.mp4");
+%v = VideoReader("video1-4.mp4");
+%v = VideoReader("video1-5.mp4");
+%v = VideoReader("video1-6.mp4");
+%v = VideoReader("video1-7.mp4");
+%v = VideoReader("video1-8.mp4");
+%v = VideoReader("video1-9.mp4");
+%v = VideoReader("video1-10.mp4");
+%v = VideoReader("video1-11.mp4");
+%v = VideoReader("video1-12.mp4");
+%v = VideoReader("video1-13.mp4");
+v = VideoReader("video1-14.mp4");
 
 frames = read(v,[1,inf]);
-%automatitazició
-siz = size(frames);
-sip = [siz(1);siz(2);siz(4)];
-
-grup1 = zeros(sip);
-copia = zeros(siz);
-
-for i = 1:siz(4)
-   copia(:,:,:,i) = frames(:,:,:,i);
+tamany = size(frames);
+copia = frames;
+grup1 = zeros(tamany(1),tamany(2), tamany(4));
+for i = 1:tamany(4)
    grup1(:,:,i) = rgb2gray(frames(:,:,:,i));
 end
 
+%% Homografia bird's eye --> birdeye
+% Càlcul background de la homografia
+base = copia(:,:,:,1);
+imshow(base);
+[x1, y1] = ginput(4);
+close
+x2 = [1;1;640;640];
+y2 = [1;352;1;352];
+M12 = [];   
+
+for i=1:4
+    M12 = [ M12;
+    x1(i) y1(i) 1 0 0 0 -x2(i)*x1(i) -x2(i)*y1(i) -x2(i);
+    0 0 0 x1(i) y1(i) 1 -y2(i)*x1(i) -y2(i)*y1(i) -y2(i)];  
+end
+
+[u,s,v] = svd( M12 );
+H12 = reshape( v(:,end), 3, 3 )';
+H12 = H12 / H12(3,3);
+H21 = inv(H12);
+
+
+memoria = zeros(12,3, 'double');
+
+
+for i = 1:4
+    imshow(base,[])
+    [x, y, b] = ginput(1);
+    close
+    p = H12*[x y 1]';
+    p = p/p(3);
+    memoria(i,:) = p;
+end
+
+
+xplim = [floor(min(memoria(:,1))), ceil(max(memoria(:,1)))];
+yplim = [floor(min(memoria(:,2))), ceil(max(memoria(:,2)))];
+
+birdeye = uint8(zeros(abs(xplim(2)) + abs(xplim(1)) + 1, size(frames, 2), 3, size(frames, 4)));
+
+[a1,a2, a3, a4] = size(birdeye);
+
+for z = 1:a4
+    for i = 1:a2
+        for j = 1:a1
+            
+            % xp i jp generals en tota la iteracio
+            xp = j + xplim(1) + 1;
+            yp = i + yplim(1) + 1;
+
+            % coloquem la imatge 1
+            p = H21*[xp yp 1]';
+            p = p/p(3);
+            x = round(p(1));
+            y = round(p(2));
+            if(x > 0 && x <= a2 && y > 0 && y <= size(base,1))
+                birdeye(j,i,:,z) = frames(y,x,:,z);
+            end
+        end
+    end
+end
+
+
+grup2 = zeros(a1,a2, a4);
+for i = 1:a4
+   grup2(:,:,i) = rgb2gray(birdeye(:,:,:,i));
+end
 
 %% TASCA 2 - SISTEMA DE CAPTACIÓ DELS OBJECTES
-%
-%
+% Agafem els 5 primers frames per fer el train i 
+% el backgorund substracting
 
 % EXTRACCIO AMB MANUAL 1
 
-mitjana = mean(grup1, 3);
+base = mean(grup1(:,:,1:10), 3);
+base2 = mean(grup2(:,:,1:10), 3);
 desv = std(grup1, [],  3);
+desv2 = std(grup2, [],  3);
 
 
-vermells = zeros(sip);
-substraccio = zeros(sip);
+vermells = zeros(tamany(1),tamany(2), tamany(4));
+vermells2 = zeros(a1, a2, a4);
+substraccio = zeros(tamany(1),tamany(2), tamany(4));
+substraccio2 = zeros(a1, a2, a4);
 
 alpha = 1;
-beta = 27;
+beta = 47;
 
 
 
-for g = 1:siz(4)
-    substraccio(:,:,g) = abs(mitjana - grup1(:,:,g)) > (desv * alpha + beta);
+for g = 1:tamany(4)
+    substraccio(:,:,g) = abs(base - grup1(:,:,g)) > (desv * alpha + beta);
+    substraccio2(:,:,g) = abs(base2 - grup2(:,:,g)) > (desv2 * alpha + beta);
 end
 
 % EXTRET PER SEPARAR ELS OBJECTIUS DEL PROJECTE DE DINS DEL FOR --> [a,b,vermells(:,:,g)] = getverm(a, b, substraccio(:,:,g));
+
+
+
 
 
 %% TASCA 3 - SISTEMA DE DETECCIÓ DE LA TRAJECTORIA I DEFINIR EL SEU RECORREGUT
@@ -61,84 +144,118 @@ end
 a = 0;
 b = 0;
 p = [];
-
-for g = 1:siz(4)
+z2 = 0;
+b2 = 0;
+p2 = [];
+for g = 1:tamany(4)
     [a,b,p, vermells(:,:,g), copia(:,:,1,g)] = getverm(a, b, p, substraccio(:,:,g),copia(:,:,1,g));
+    [z2,b2,p2, vermells2(:,:,g), birdeye(:,:,1,g)] = getverm(z2, b2, p2, substraccio2(:,:,g),birdeye(:,:,1,g));
 end
-
 
 
 %% TASCA 4 - SISTEMA PER REPRESENTAR LA TRAJECTORIA SOBRE EL VÍDEO
 %
 % HEM ESTAT PENSANT FER LA INCORPORACIÓ PRÈVIA, A LA VEGADA QUE DETECTEM I
 % DEFINIM LA TRAJECTÒRIA PER EVITAR ALTA REDUNDANCIA I REPETICIÓ DE BUCLES 
-%
+%   
 
-vermells2 = zeros(siz);
+vermellsfinal = zeros(tamany);
+vermellsfinal2 = zeros(a1,a2,a3,a4);
 
 
-for x = 1:siz(4)
+for x = 1:a4
     if x ~= 1
-        vermells2(:,:,1,x) = vermells2(:,:,1,x-1) + vermells(:,:,x);
+        vermellsfinal(:,:,1,x) = vermellsfinal(:,:,1,x-1) + vermells(:,:,x);
+        vermellsfinal2(:,:,1,x) = vermellsfinal2(:,:,1,x-1) + vermells2(:,:,x);
     else
-        vermells2(:,:,1,1) = vermells(:,:,1);
+        vermellsfinal(:,:,1,1) = vermells(:,:,1);
+        vermellsfinal2(:,:,1,1) = vermells2(:,:,1);
     end
 end
     
-%% TASCA 5 - SISTEMA DE GENERACIÓ DE LA TRAJECTÒRIA
-%
-%
-
-moviment = [p(1);p(2); p(3)];
-tamany = size(p);
-
-%llistes de prediccions 
-prediccio = zeros(tamany);
-prediccio(1:3,:) = p(1:3,:);
-
-for x = 4:tamany(1)
-    
-    d1 = [moviment(2:1)-moviment(1:1) ; moviment(2:2)-moviment(1:2)];
-    d2 = [moviment(3:1)-moviment(2:1) ; moviment(3:2)-moviment(2:2)];
-    
-    prox_valors = [d2(1)-d1(1);d2(2)-d2(2)];
-    
-    next = moviment(3,1);
-    
-    %distancies anteriors
-    moviment = [moviment(x-3:x-1,:);p(x, :)];
-end
-
-
+birdeye2 = incorporar(p2, birdeye);
+copia2 = incorporar(p, copia);
+[imtest2,birdeye3] = canvis_direccio(p2, birdeye2);
+[imtest,copia2] = canvis_direccio(p, copia2);
 %% TASCA FINAL - GUARDAT DEL VIDEO
 %
 %  Aquesta part simplement es utilitzada per generar el video final i aixi
 %  fer un anàlisi de la solució obtinguda
 %
 
-video = VideoWriter('C:\Users\ernu1\Desktop\Universitat\PSIV\PROJECTE\video_s.mp4','MPEG-4');
-video2 = VideoWriter('C:\Users\ernu1\Desktop\Universitat\PSIV\PROJECTE\video_p.mp4','MPEG-4');
-video3 = VideoWriter('C:\Users\ernu1\Desktop\Universitat\PSIV\PROJECTE\video_c.mp4','MPEG-4');
-open(video);
-open(video2);
-open(video3);
+videot = VideoWriter('C:\Users\ernu1\Desktop\Universitat\PSIV\PROJECTE\video_trajectoria.mp4','MPEG-4');
+videot2 = VideoWriter('C:\Users\ernu1\Desktop\Universitat\PSIV\PROJECTE\video_trajectoria2.mp4','MPEG-4');
 
-img = zeros(sip(1), sip(2));
-img2 = zeros(sip);
-img3 = zeros(sip); 
+open(videot);
+open(videot2);
 
-for i =1:374
-    img = substraccio(:,:,i);
-    img2 = vermells2(:,:,:,i)/255;
-    img3 = copia(:,:,:,i)/255;
-    writeVideo(video, double(img));
-    writeVideo(video2, img2);
-    writeVideo(video3, img3);
-end
-    
-close(video);
-close(video2);
-close(video3);
+vermell = cast(vermellsfinal, 'uint8');
+vermell2 = cast(vermellsfinal2, 'uint8');
+
+writeVideo(videot, vermell);
+writeVideo(videot2, vermell2);
+
+close(videot);
+close(videot2);
+
+
+% part 1
+
+
+videoc = VideoWriter('C:\Users\ernu1\Desktop\Universitat\PSIV\PROJECTE\video_compost.mp4','MPEG-4');
+videoc2 = VideoWriter('C:\Users\ernu1\Desktop\Universitat\PSIV\PROJECTE\video_compost2.mp4','MPEG-4');
+videoc3 = VideoWriter('C:\Users\ernu1\Desktop\Universitat\PSIV\PROJECTE\video_compost3.mp4','MPEG-4');
+videoc4 = VideoWriter('C:\Users\ernu1\Desktop\Universitat\PSIV\PROJECTE\video_compost4.mp4','MPEG-4');
+
+open(videoc);
+open(videoc2);
+open(videoc3);
+open(videoc4);
+
+writeVideo(videoc, copia);
+writeVideo(videoc2, copia2);
+writeVideo(videoc3, birdeye);
+writeVideo(videoc4, birdeye3);
+
+close(videoc);
+close(videoc2);
+close(videoc3);
+close(videoc4);
+
+
+videof = VideoWriter('C:\Users\ernu1\Desktop\Universitat\PSIV\PROJECTE\video_final.mp4','MPEG-4');
+videof2 = VideoWriter('C:\Users\ernu1\Desktop\Universitat\PSIV\PROJECTE\video_final2.mp4','MPEG-4');
+videof3 = VideoWriter('C:\Users\ernu1\Desktop\Universitat\PSIV\PROJECTE\video_final3.mp4','MPEG-4');
+videof4 = VideoWriter('C:\Users\ernu1\Desktop\Universitat\PSIV\PROJECTE\video_final4.mp4','MPEG-4');
+
+open(videof);
+open(videof2);
+open(videof3);
+open(videof4);
+
+fin = copia2 + vermell;
+fin2 = birdeye3 + vermell2;
+
+writeVideo(videof3, fin);
+writeVideo(videof4, fin2);
+
+imtest = im2uint8(imtest);
+imtest2 = im2uint8(imtest2);
+
+fin  =  fin + imtest;
+fin2  =  fin2 + imtest2;
+
+fin = fin + vermell;
+fin2 = fin2 + vermell2;
+
+writeVideo(videof, fin);
+writeVideo(videof2, fin2);
+
+close(videof);
+close(videof2);
+
+close(videof3);
+close(videof4);
 
 %% FUNCIONS DEL CODI
 
@@ -148,10 +265,11 @@ function [a,b,p,sortida,copia] = getverm(a, b,p,img,copia)
 totalx = 0;
 totaly = 0;
 count = 0;
+[v1,v2,v3,v4] = size(copia);
 % CALCUL DEL CENTRE DE MASSES DE L'OBJECTE
-siz = size(img);
-for x = 1:siz(1)
-    for y = 1:siz(2)
+
+for x = 1:v1
+    for y = 1:v2
         if img(x,y) == 1
         totalx = totalx + x;
         totaly = totaly + y;
@@ -160,15 +278,16 @@ for x = 1:siz(1)
     end
 end
 
-sortida = zeros(siz);
+sortida = zeros(v1, v2);
 
-% FEFINICIÓ TRAJECTÒRIA .
+% DETECCIÓ D'OBJECTE EN IMATGE.
+x = 0;
+y = 0;
+
 if count ~= 0
     x = round(totalx/count);
     y = round(totaly/count);
     
-    % GUARDEM ELS PUNTS PER DESPRES GENERAR LA PREDICCIÓ
-    p = [p;[x,y]];
     if a == 0
         a = x;
         b = y;
@@ -178,7 +297,7 @@ if count ~= 0
     
     while (x ~= a || y ~= b )
         sortida(a,b) = 255;
-        copia(a,b) = 255;
+        copia(a,b) = 255;   
         if (x ~= a)
             res_x = x - a;
             res_x = res_x / abs(res_x);
@@ -196,12 +315,55 @@ if count ~= 0
             b = b + res_y;
         end
     end
-        
-        
-else
+else 
     a = 0;
-    b = 0;
+    b = 0;     
 end
+p = [p;[x,y]];
 end
 
+function matriu = incorporar(p, matriu)
+    [pa1, pa2] = size(p);
+    for i = 1:pa1
+        if p(i) ~= 0
+            matriu(:,:,:,i) = insertShape(matriu(:,:,:,i), "Circle", [p(i,2),p(i,1) , 30]);
+        end
+    end
+end
+
+
+
+function [img, matriu] = canvis_direccio(p, matriu)
+    signe1 = 0;
+    signe2 = 0;
+    tam = size(matriu);
+    img = zeros(tam(1),tam(2), tam(3));
+    anterior = [0, 0];
+    [pa1, pa2] = size(p);
+    for i = 1:pa1
+        if p(i) ~= 0
+            if anterior(1) ~= 0
+               if signe1 == 0
+                   signe1 = (p(i,1) -  anterior(1)) / abs(p(i,1) -  anterior(1));
+                   signe2 = (p(i,2) -  anterior(2)) / abs(p(i,2) -  anterior(2));
+               else
+                   signen1 = (p(i,1) -  anterior(1)) / abs(p(i,1) -  anterior(1));
+                   signen2 = (p(i,2) -  anterior(2)) / abs(p(i,2) -  anterior(2));
+
+                   if signe1 ~= signen1 || signe2 ~= signen2
+                       matriu(:,:,:,i) = insertShape(matriu(:,:,:,i), "FilledCircle", [p(i,2),p(i,1) , 15]);
+                       img = insertShape(img, "FilledCircle", [p(i,2),p(i,1) , 15]);
+                       signe1 = signen1;
+                       signe2 = signen2;
+                   end
+               end
+            end
+
+            anterior = p(i, :);
+            
+        end
+    end
+            
+
+end
 
